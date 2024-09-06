ec65bcd948446a39885b09cf74707f466b57c3ba
StephenL <stephen.lloyd@manchester.ac.uk>
2024-09-06 14:29:26 +0100

    initial methods to build from

--- src/main/java/org/uksrc/archive/ObservationResource.java
+++ src/main/java/org/uksrc/archive/ObservationResource.java
@@ -5,31 +5,60 @@ package org.uksrc.archive;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.ivoa.dm.caom2.caom2.Observation;
+import org.ivoa.dm.caom2.caom2.SimpleObservation;
+
+import java.util.List;
 
 
-@Produces(MediaType.APPLICATION_JSON)
-@Path("/observation")
+//@Produces(MediaType.APPLICATION_JSON)
+@Path("/")
 public class ObservationResource {
-
 
     @PersistenceContext
     protected EntityManager em;  // exists for the application lifetime no need to close
 
-    @POST
-    @Operation(summary = "create a new Observation")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @PUT
+    @Path("/add")
+    @Operation(summary = "Create a new Observation")
+    @Consumes(MediaType.APPLICATION_XML)
     @Transactional
-    public Observation addObservation(Observation observation) {
+    public Observation addObservation(SimpleObservation observation) {
         em.persist(observation);
         return observation;
     }
+
+    @GET
+    @Path("/observations")
+    @Operation(summary = "Retrieve all observations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Observation> getAllObservations() {
+        TypedQuery<Observation> query = em.createQuery("SELECT o FROM Observation o", Observation.class);
+      //  query.setMaxResults(10); //TODO pagination
+        return query.getResultList();
+    }
+
+    @GET
+    @Path("/observations/{collection}")
+    @Operation(summary = "Retrieve observations from a collection")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Observation> getObservations(@PathParam("collection") String collection) {
+        TypedQuery<Observation> query = em.createQuery("SELECT o FROM Observation o WHERE o.collection = :collection", Observation.class);
+        query.setParameter("collection", collection); // Set the parameter value
+        return query.getResultList();
+    }
+
+
+//    @POST
+//    @Operation(summary = "test1")
+//    public String addSomeValue(String observation) {
+//       // em.persist(observation);
+//        return observation;
+//    }
 
 }
