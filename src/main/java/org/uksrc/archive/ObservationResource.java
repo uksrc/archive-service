dabbf4e8f2301311890a33f41feb7f51262ad0dd
StephenL <stephen.lloyd@manchester.ac.uk>
2024-09-10 16:56:33 +0100

    Some initial openAPI documentation

--- src/main/java/org/uksrc/archive/ObservationResource.java
+++ src/main/java/org/uksrc/archive/ObservationResource.java
@@ -10,6 +10,11 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.ivoa.dm.caom2.caom2.DerivedObservation;
 import org.ivoa.dm.caom2.caom2.Observation;
 import org.ivoa.dm.caom2.caom2.SimpleObservation;
@@ -25,18 +30,33 @@ public class ObservationResource {
 
     @PUT
     @Path("/add")
-    @Operation(summary = "Create a new Observation")
+    @Operation(summary = "Create a new Observation", description = "Creates a new observation in the database, note the supplied ID needs to be unique.")
+    @RequestBody(
+            description = "XML representation of the Observation",
+            required = true,
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_XML,
+                    schema = @Schema(implementation = SimpleObservation.class)
+            )
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Observation created successfully",
+            content = @Content(schema = @Schema(implementation = SimpleObservation.class))
+    )
+    @APIResponse(
+            responseCode = "400",
+            description = "Invalid input"
+    )
     @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
     @Transactional
-    public Response addObservation(SimpleObservation observation) throws WebApplicationException {
+    public Response addObservation(SimpleObservation observation) {
         try {
             em.persist(observation);
             em.flush();                 //Forced transaction to catch duplicate keys errors more gracefully.
         } catch (Exception e) {
-            //throw new WebApplicationException(e.getMessage(), Response.Status.BAD_REQUEST);
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(e.getMessage())
-                    .build();
+            return errorResponse(e);
         }
         //return observation;
         return Response.status(Response.Status.CREATED)
@@ -46,24 +66,73 @@ public class ObservationResource {
 
     @PUT
     @Path("/derived/add")
-    @Operation(summary = "Create a new Derived Observation")
+    @Operation(summary = "Create a new Derived Observation", description = "Create a DERIVED observation in the database, note ID must be unique across all observations.")
+    @RequestBody(
+            description = "XML representation of the Derived Observation",
+            required = true,
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_XML,
+                    schema = @Schema(implementation = DerivedObservation.class)
+            )
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Observation created successfully",
+            content = @Content(schema = @Schema(implementation = DerivedObservation.class))
+    )
+    @APIResponse(
+            responseCode = "400",
+            description = "Invalid input"
+    )
     @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
     @Transactional
-    public Observation addObservation(DerivedObservation observation) throws WebApplicationException {
+    public Response addObservation(DerivedObservation observation) {
         try {
             em.persist(observation);
+            em.flush();
         } catch (Exception e) {
-            throw new WebApplicationException(e.getLocalizedMessage(), Response.Status.BAD_REQUEST);
+            return errorResponse(e);
         }
-        return observation;
+        return Response.status(Response.Status.CREATED)
+                .entity(observation)
+                .build();
     }
 
     @POST
     @Path("/update/{observationId}")
-    @Operation(summary = "Update an existing Observation, based on id")
+    @Operation(summary = "Update an existing Observation", description = "Updates an existing observation with the supplied ID")
+    @Parameter(
+            name = "observationId",
+            description = "ID of the Observation to be updated",
+            required = true,
+            example = "123"
+    )
+    @RequestBody(
+            description = "XML representation of the Derived Observation",
+            required = true,
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_XML,
+                    schema = @Schema(implementation = Observation.class)
+            )
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Observation updated successfully",
+            content = @Content(schema = @Schema(implementation = Observation.class))
+    )
+    @APIResponse(
+            responseCode = "404",
+            description = "An Observation with the supplied ID has not been found."
+    )
+    @APIResponse(
+            responseCode = "400",
+            description = "Invalid input"
+    )
     @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
     @Transactional
-    public Response updateObservation(@PathParam("observationId") String id, SimpleObservation observation) throws WebApplicationException {
+    public Response updateObservation(@PathParam("observationId") String id, SimpleObservation observation) {
         try {
             //Only update IF found
             Observation existing = em.find(Observation.class, id);
@@ -73,7 +142,7 @@ public class ObservationResource {
                 return Response.ok(observation).build();
             }
         } catch (Exception e) {
-            throw new WebApplicationException(e.getLocalizedMessage(), Response.Status.BAD_REQUEST);
+            return errorResponse(e);
         }
 
         return Response.status(Response.Status.NOT_FOUND)
@@ -105,7 +174,7 @@ public class ObservationResource {
     @Path("/delete/{observationId}")
     @Operation(summary = "Delete an existing observation")
     @Transactional
-    public Response deleteObservation(@PathParam("observationId") String id) throws WebApplicationException {
+    public Response deleteObservation(@PathParam("observationId") String id) {
         try {
             Observation observation = em.find(Observation.class, id);
             if (observation != null) {
@@ -113,11 +182,17 @@ public class ObservationResource {
                 return Response.status(Response.Status.NO_CONTENT).build();
             }
         } catch (Exception e) {
-            throw new WebApplicationException(e.getLocalizedMessage(), Response.Status.BAD_REQUEST);
+            return errorResponse(e);
         }
 
         return Response.status(Response.Status.NOT_FOUND)
                 .entity("Observation with ID " + id + " not found")
+                .build();
+    }
+
+    private Response errorResponse (Exception e){
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(e.getMessage())
                 .build();
     }
 }
