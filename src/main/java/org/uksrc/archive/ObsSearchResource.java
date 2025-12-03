23154146fb4f0f0a6b86e2bb8d0eb0de2c9469ec
StephenL <stephen.lloyd@manchester.ac.uk>
2025-12-03 09:41:42 +0000

    Forced the return type of the pgsphere function to be double
    added paging to the cone search API

--- src/main/java/org/uksrc/archive/ObsSearchResource.java
+++ src/main/java/org/uksrc/archive/ObsSearchResource.java
@@ -10,8 +10,8 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.ivoa.dm.caom2.Observation;
-
-import java.util.List;
+import org.uksrc.archive.utils.responses.Responses;
+import org.uksrc.archive.utils.tools.Tools;
 
 @Path("/search")
 public class ObsSearchResource {
@@ -20,21 +20,28 @@ public class ObsSearchResource {
     protected EntityManager em;
 
    public static final String CONE_SEARCH_QUERY =
-           "SELECT o FROM Observation o JOIN o.targetPosition tp JOIN tp.coordinates p" +
-                   " WHERE pgsphere_check_distance(p.cval1, p.cval2, :ra, :dec) <= :radiusInDegrees";
+           "SELECT obs FROM Observation obs JOIN obs.targetPosition tp JOIN tp.coordinates p" +
+                   " WHERE FUNCTION('pgsphere_distance', p.cval1, p.cval2, :ra, :dec) <= radians(:radiusInDegrees)";
 
     @GET
     @Path("/cone")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getAllObservations(@QueryParam("ra") Double ra, @QueryParam("dec") Double dec, @QueryParam("radius") Double radius) {
+    public Response getAllObservations(@QueryParam("ra") Double ra, @QueryParam("dec") Double dec, @QueryParam("radius") Double radius,
+                                       @QueryParam("page") Integer page, @QueryParam("size") Integer size) {
+        if ((page != null) ^ (size != null)) {
+            return Responses.errorResponse("Both 'page' and 'size' must be provided together or neither.");
+        } else if ((page != null && page < 0) || (size != null && size < 1)) {
+            return Responses.errorResponse("Page must be 0 or greater and size must be greater than 0.");
+        }
+
+
         TypedQuery<Observation> query = em.createQuery(CONE_SEARCH_QUERY, Observation.class);
         query.setParameter("ra", ra);
         query.setParameter("dec", dec);
         query.setParameter("radiusInDegrees", radius);
 
         try {
-            List<Observation> observations = query.getResultList();
-            return Response.ok(observations).build();
+            return Tools.performQuery(page, size, query);
         } catch (Exception e) {
             System.err.println("Query Execution Error: " + e.getMessage());
             return Response.serverError().entity("Database query failed.").build();
--- src/main/java/org/uksrc/archive/utils/query/PgSphereDialect.java
+++ src/main/java/org/uksrc/archive/utils/query/PgSphereDialect.java
@@ -4,6 +4,7 @@ import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.boot.model.FunctionContributor;
 import org.hibernate.dialect.PostgreSQLDialect;
 import org.hibernate.query.sqm.function.SqmFunctionRegistry;
+import org.hibernate.type.StandardBasicTypes;
 
 /**
  * Class to register pgSphere helper functions for Hibernate queries.
@@ -14,11 +15,17 @@ public class PgSphereDialect extends PostgreSQLDialect implements FunctionContri
     public void contributeFunctions(FunctionContributions functionContributions) {
         SqmFunctionRegistry registry = functionContributions.getFunctionRegistry();
 
+        var typeConfig = functionContributions.getTypeConfiguration();
+        var doubleType = typeConfig.getBasicTypeRegistry()
+                .resolve(StandardBasicTypes.DOUBLE);
+
         // Distance function x,y <-> x,y
         registry.registerPattern(
-                "pgsphere_check_distance",
-                "spoint(radians(?1), radians(?2))::spoint <-> spoint(radians(?3), radians(?4))::spoint"
+                "pgsphere_distance",
+                "(spoint(radians(?1), radians(?2))::spoint <-> spoint(radians(?3), radians(?4))::spoint)",
+                doubleType
         );
+
     }
 }
 
