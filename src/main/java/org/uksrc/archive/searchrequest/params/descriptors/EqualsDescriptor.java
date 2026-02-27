1efbf90023d052a080ea2709c35aa60a1b52108a
StephenL <stephen.lloyd@manchester.ac.uk>
2026-02-27 15:18:41 +0000

    Descriptive headers added

--- src/main/java/org/uksrc/archive/searchrequest/params/descriptors/EqualsDescriptor.java
+++ src/main/java/org/uksrc/archive/searchrequest/params/descriptors/EqualsDescriptor.java
@@ -1,5 +1,6 @@
 package org.uksrc.archive.searchrequest.params.descriptors;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 import org.uksrc.archive.searchrequest.query.QueryContext;
