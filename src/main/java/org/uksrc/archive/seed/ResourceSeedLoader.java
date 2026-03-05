afa65761cf0d307fb738badaff9ffa6005bd737d
StephenL <stephen.lloyd@manchester.ac.uk>
2026-03-05 16:19:09 +0000

    Should now load any XML file in the specified folder, which will give the ability to dynamically add/remove files as required

--- src/main/java/org/uksrc/archive/seed/ResourceSeedLoader.java
+++ src/main/java/org/uksrc/archive/seed/ResourceSeedLoader.java
@@ -13,9 +13,14 @@ import org.ivoa.dm.caom2.Observation;
 import org.uksrc.archive.ObservationResource;
 
 import java.io.InputStream;
-import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  *  Loads example resource XML files and loads them into the database upon startup.
@@ -34,33 +39,63 @@ public class ResourceSeedLoader {
     @ConfigProperty(name = "testdata.seed.enabled", defaultValue = "false")
     boolean enabled;
 
-    //List of example resource files to load on startup
-    //Hard-coded to avoid potential classpath resolution issues when iterating over files in a named folder.
-    private static final List<String> FILES = List.of(
-            "seed/observation-example1.xml",
-            "seed/observation-example2.xml",
-            "seed/observation-example3.xml",
-            "seed/observation-example4.xml",
-            "seed/observation-example5.xml"
-    );
+    private static final String FOLDER = "seed";   // The folder to search for XML files, needs to be on the classpath.
 
     /**
-     * Loads examples from file(s) and deploys them to the database.
+     * Loads and processes XML files containing observation data from a predefined folder.
+     * If enabled, this method retrieves all XML files in the specified folder, parses each
+     * file into an {@code Observation} object, and adds the observation to the
+     * {@code observationResource}. Any exceptions encountered during the process are
+     * logged to prevent interruption of the service.
      */
     @PostConstruct
     @Transactional
     void load() {
         if (enabled) {
-            for (String file : FILES) {
-                try {
+            try {
+                List<String> files = findXmlFiles(FOLDER);
+                for (String file : files) {
                     Observation obs = readXmlFile(file, Observation.class);
                     observationResource.addObservation(obs);
-                } catch (Exception e) {
-                    //Don't block execution of the service if an example resource is missing
-                    System.out.println(e.getMessage());
+                }
+            } catch (Exception e) {
+                //Warn of error to stop blocking of the service itself.
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Finds and retrieves the relative paths of all XML files within a given folder
+     * accessible through the class loader.
+     *
+     * @param folder The name of the folder to search for XML files. This should be a
+     *               resource folder accessible within the application classpath.
+     * @return A list of relative paths to the XML files found within the specified folder.
+     * @throws Exception If an error occurs while accessing the resources or reading files.
+     */
+    public static List<String> findXmlFiles(String folder) throws Exception {
+
+        List<String> files = new ArrayList<>();
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+
+        Enumeration<URL> resources = cl.getResources(folder);
+
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+
+            if (url.getProtocol().equals("file")) {
+                Path path = Paths.get(url.toURI());
+
+                try (Stream<Path> stream = Files.list(path)) {
+                    stream.filter(p -> p.toString().endsWith(".xml"))
+                            .forEach(p -> files.add(folder + "/" + p.getFileName()));
                 }
             }
         }
+
+        return files;
     }
 
     /**
