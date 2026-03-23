e7ba3fa492a478c767f4d658827d836d63ca4a5a
StephenL <stephen.lloyd@manchester.ac.uk>
2026-03-23 13:14:17 +0000

    function headers added

--- src/main/java/org/uksrc/archive/searchrequest/params/descriptors/StringArrayDescriptor.java
+++ src/main/java/org/uksrc/archive/searchrequest/params/descriptors/StringArrayDescriptor.java
@@ -8,6 +8,19 @@ import org.uksrc.archive.searchrequest.params.parser.FieldRegistry.FieldDefiniti
 import org.uksrc.archive.searchrequest.params.parser.FieldRegistry.FieldType;
 import org.uksrc.archive.searchrequest.query.QueryContext;
 
+/**
+ * This class is an implementation of the {@link PredicateDescriptor} interface
+ * and provides logic to create JPA query predicates specifically for handling
+ * string array fields in a database. It is designed to process field definitions
+ * with the type {@link FieldType#STRING_ARRAY} and apply case-insensitive matching.
+ *
+ * <p>The resulting predicate is constructed using the JPA Criteria API, allowing dynamic,
+ * type-safe query generation.
+ *
+ * <p>Utilises:
+ * - The {@link QueryContext} to access the {@link CriteriaBuilder} and root entity for the query.
+ * - The {@link FieldDefinition} to extract field metadata such as the field type and entity path.
+ */
 public class StringArrayDescriptor implements PredicateDescriptor {
 
     private final FieldDefinition fieldDef;
@@ -18,6 +31,16 @@ public class StringArrayDescriptor implements PredicateDescriptor {
         this.value = value.toUpperCase();
     }
 
+    /**
+     * Constructs a JPA predicate to filter query results based on the field definition
+     * and value provided within the context of the given {@link QueryContext}.
+     * This particular method searches for values in a value separated string.
+     *
+     * @param ctx the {@link QueryContext} that provides access to the {@link CriteriaBuilder}
+     *            and root entity for predicate construction.
+     * @return a {@link Predicate} that represents the filtering condition based on
+     *         the field definition and input value.
+     */
     @Override
     public Predicate toPredicate(QueryContext<?> ctx) {
         CriteriaBuilder cb = ctx.criteriaBuilder();
@@ -32,10 +55,8 @@ public class StringArrayDescriptor implements PredicateDescriptor {
             Expression<String> paddedColumn = cb.upper(
                     cb.concat("#", cb.concat(fieldPath, "#"))
             );
-
             return cb.like(paddedColumn, "%" + searchTerm + "%");
         }
-
         return cb.equal(cb.upper(fieldPath), value.toUpperCase());
     }
 }
