56cb7be99374feda17b2a1363a2a236a9219e59c
StephenL <stephen.lloyd@manchester.ac.uk>
2026-03-23 16:38:23 +0000

    Added function header for search query

--- src/main/java/org/uksrc/archive/ObsSearchResource.java
+++ src/main/java/org/uksrc/archive/ObsSearchResource.java
@@ -40,11 +40,114 @@ public class ObsSearchResource {
    public static final String CONE_SEARCH_QUERY =
            "SELECT obs FROM Observation obs JOIN obs.targetPosition tp JOIN tp.coordinates p" +
                    " WHERE FUNCTION('pgsphere_distance', p.cval1, p.cval2, :ra, :dec) <= radians(:radiusInDegrees)";
-    @Inject
-    Request request;
 
     @GET
     @Path("/")
+    @Operation(summary = "Search of Observations", description = "Returns a list of Observations that match the supplied parameter values.")
+    @Parameters({
+            @Parameter(
+                    name = "ra",
+                    description = "Right ascension in degrees",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.NUMBER, format = "double")
+            ),
+            @Parameter(
+                    name = "dec",
+                    description = "Declination in degrees",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.NUMBER, format = "double")
+            ),
+            @Parameter(
+                    name = "radius",
+                    description = "Radius in degrees",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.NUMBER, format = "double")
+            ),
+            @Parameter(
+                    name = "project",
+                    description = "Project name",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING)
+            ),
+            @Parameter(
+                    name = "telescope",
+                    description = "Telescope name",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING)
+            ),
+            @Parameter(
+                    name = "instrument",
+                    description = "Instrument name",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING)
+            ),
+            @Parameter(
+                    name = "target",
+                    description = "Target name",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING)
+            ),
+            @Parameter(
+                    name = "band",
+                    description = "Energy band",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING)
+            ),
+            @Parameter(
+                    name = "freqMin",
+                    description = "Minimum frequency (for use in a range)",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.NUMBER, format = "double")
+            ),
+            @Parameter(
+                    name = "freqMax",
+                    description = "Maximum frequency (for use in a range)",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.NUMBER, format = "double")
+            ),
+            @Parameter(
+                    name = "dateMin",
+                    description = "Minimum date (for use in a range)",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING, format = "ISO8601")
+            ),
+            @Parameter(
+                    name = "dateMax",
+                    description = "Maximum date (for use in a range)",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING, format = "ISO8601")
+            ),
+            @Parameter(
+                    name = "startDate",
+                    description = "Returns any observation active beyond this date",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.STRING, format = "ISO8601")
+            ),
+            @Parameter(
+                    name = "page",
+                    description = "The page number to retrieve, zero-indexed. If not provided, ALL results are returned.",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.INTEGER, minimum = "0")
+            ),
+            @Parameter(
+                    name = "size",
+                    description = "The number of observations per page. If not provided, ALL results are returned.",
+                    in = ParameterIn.QUERY,
+                    schema = @Schema(type = SchemaType.INTEGER, minimum = "1")
+            )
+    })
+    @APIResponse(
+            responseCode = "200",
+            description = "List of observations retrieved successfully",
+            content = {
+                    @Content(
+                            mediaType = MediaType.APPLICATION_XML, schema = @Schema(oneOf = ObservationListWrapper.class)
+                    ),
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON, schema = @Schema(oneOf = ObservationListWrapper.class)
+                    )
+            }
+    )
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @ConditionalRolesAllowed("resource.roles.view")
     public Response search(@Context UriInfo uriInfo) {
