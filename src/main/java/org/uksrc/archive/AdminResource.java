90737aa4a0fc7ec3d08325c5aff3daeba204776e
StephenL <stephen.lloyd@manchester.ac.uk>
2026-04-01 12:23:04 +0100

    Added functionality to the admin resource to allow an admin user to insert an observation resource
    Added default credentials for an admin user in test profile

--- src/main/java/org/uksrc/archive/AdminResource.java
+++ src/main/java/org/uksrc/archive/AdminResource.java
@@ -3,69 +3,115 @@ package org.uksrc.archive;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.ivoa.dm.caom2.DerivedObservation;
 import org.ivoa.dm.caom2.Observation;
-import org.uksrc.archive.utils.responses.Responses;
+import org.ivoa.dm.caom2.SimpleObservation;
 import org.uksrc.archive.utils.tools.Tools;
 
 @Path("/admin")
 @RolesAllowed("admin")
 public class AdminResource {
 
-  ///  @PersistenceContext
-  //  protected EntityManager em;
+    @PersistenceContext
+    protected EntityManager em;
 
     @GET
-    @RolesAllowed("admin")
     public String test() {
-        return "ok";
+        return "Admin utilities";
     }
 
-    /**
-     * Adds an observation to the database
-     * @param observation Either a SimpleObservation or a DerivedObservation
-     * @return Response containing status code and added observation (if successful)
-     */
-  /*  private Response submitObservation(Observation observation) {
-        if (findObservation(observation.getId()) != null) {
-            return Responses.errorResponse("Observation.id " + observation.getId() + " already exists.");
-        }
-
-        try {
-            em.persist(observation);
-            em.flush();
-
-            Object formattedObs = Tools.formatObservation(observation);
-            return Response.status(Response.Status.CREATED)
-                    .entity(formattedObs)
-                    .build();
-
-        } catch (Exception e) {
-            return Responses.errorResponse(e);
-        }
-    }*/
-
-    /**
-     * Checks to see if an observation with the supplied ID already exists
-     * @param id Observation.id
-     * @return The observation if found, null if not
-     */
-  /*  private Observation findObservation(String id) {
-        TypedQuery<Observation> existsQuery = em.createQuery(
-                "SELECT o FROM Observation o WHERE o.id = :id", Observation.class
-        );
-
-        try {
-            existsQuery.setParameter("id", id);
-            return existsQuery.getSingleResult();
-        } catch (NoResultException e){
-            return null;
-        }
-    }*/
+    @POST
+    @Path("/addObservation")
+    @Operation(summary = "Create a new Observation", description = "Creates a new observation in the database, note the supplied ID needs to be unique and XML namespace/JSON type supplied.")
+    @RequestBody(
+            description = "XML representation of the Observation.",
+            required = true,
+            content = {
+                    @Content(
+                            mediaType = MediaType.APPLICATION_XML,
+                            schema = @Schema(oneOf = {SimpleObservation.class, DerivedObservation.class}),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "XML Example - SimpleObservation",
+                                            value = """
+                                                    <SimpleObservation xmlns:caom2="http://ivoa.net/dm/models/vo-dml/experiment/caom2"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="caom2:caom2.SimpleObservation">
+                                                    <collection>test</collection>
+                                                    <uri>https://observatory.org/observations/CY9004_C_001_20200721</uri>
+                                                    <intent>science</intent>
+                                                    </SimpleObservation>"""
+                                    ),
+                                    @ExampleObject(
+                                            name = "XML Example - DerivedObservation",
+                                            value = """
+                                                    <DerivedObservation xmlns:caom2="http://ivoa.net/dm/models/vo-dml/experiment/caom2"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="caom2:caom2.DerivedObservation">
+                                                    <collection>test</collection>
+                                                    <uri>https://observatory.org/observations/CY9004_C_001_20200722</uri>
+                                                    <intent>science</intent>
+                                                    <members>jbo-simple1</members>
+                                                    <members>jbo-simple2</members>
+                                                    </DerivedObservation>"""
+                                    )
+                            }
+                    ),
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON,
+                            schema = @Schema(oneOf = {SimpleObservation.class, DerivedObservation.class}),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "JSON Example - SimpleObservation",
+                                            value = """
+                                                     {
+                                                        "@type": "caom2:caom2.SimpleObservation",
+                                                        "collection": "test",
+                                                        "uri": "https://observatory.org/observations/CY9004_C_001_20200721",
+                                                        "intent": "science",
+                                                    }
+                                                    """
+                                    ),
+                                    @ExampleObject(
+                                            name = "JSON Example - DerivedObservation",
+                                            value = """
+                                                    {
+                                                        "@type": "caom2:caom2.DerivedObservation",
+                                                        "collection": "test",
+                                                        "uri": "https://observatory.org/observations/CY9004_C_001_20200722",
+                                                        "intent": "science",
+                                                        "members": [
+                                                            "jbo-simple1",
+                                                            "jbo-simple2"
+                                                        ]
+                                                    }
+                                                """
+                                    )
+                            }
+                    )
+            }
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Observation created successfully",
+            content = @Content(schema = @Schema(oneOf = {SimpleObservation.class, DerivedObservation.class}))
+    )
+    @APIResponse(
+            responseCode = "400",
+            description = "Invalid input"
+    )
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Transactional
+    public Response submitObservation(Observation observation) {
+        return Tools.submitObservation(em, observation);
+    }
 }
 
